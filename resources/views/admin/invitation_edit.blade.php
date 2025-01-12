404552807a7a868996dfb1c9a4ac7f1faa7f9fe7
hnndhr <hananadhira1475@gmail.com>
2025-01-12 17:00:11 +0700

    Invitiation_edit

--- resources/views/admin/invitation_edit.blade.php
+++ resources/views/admin/invitation_edit.blade.php
@@ -10,30 +10,14 @@
 
 <body class="bg-[#F7FCF5] font-mont">
   <!-- Navigation Bar -->
-  <div class="fixed top-0 left-0 right-0 ht grid grid-cols-[70%_30%] px-4 py-5 bg-green-main">
+  <div class="fixed top-0 left-0 right-0 ht grid grid-cols-[80%_20%] px-4 py-5 bg-green-main">
     <div class="flex text-left text-xl font-bold items-center text-brown-enzo">
       <h1>Enzo Group</h1>
     </div>
-    <div class="grid grid-cols-3 gap-1 font-medium">
-      <a href="" class="text-brown-enzo flex flex-col justify-center items-center group mr-7">Dashboard
+    <div class="grid grid-cols-2 font-medium">
+      <a href="" class="text-brown-enzo flex flex-col justify-center items-center group mr-0">Dashboard
           <div class="bg-brown-enzo h-[2px] w-0 group-hover:w-[90%] transition-all duration-500"></div>
       </a>
-
-      <!-- Dropdown Menu -->
-      <div class="flex flex-col justify-center items-center group relative">
-        <!-- Dropdown Button -->
-        <button class="text-brown-enzo flex flex-col justify-center items-center">Form Order
-          <div class="bg-brown-enzo h-[2px] w-0 group-hover:w-full transition-all duration-500"></div>
-        </button>
-
-        <!-- Dropdown Content -->
-        <div class="absolute opacity-0 group-hover:opacity-100 bg-green-light shadow-lg mt-2 rounded-md z-10 top-full left-5 w-50 transition-opacity duration-500 delay-25">
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-cream rounded-md">Invitation</a>
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-cream rounded-md">Souvenir</a>
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-cream rounded-md">Seminar Kit</a>
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-cream rounded-md">Packaging</a>
-        </div>
-      </div>
 
       <a href="" class="text-brown-enzo flex flex-col justify-center items-center group">Admin
           <div class="bg-brown-enzo h-[2px] w-0 group-hover:w-[90%] transition-all duration-500"></div>
@@ -344,14 +328,127 @@
         </div>
       </div>
 
-      <!-- Submit Button -->
-      <button type="submit"
-        class="bg-brown-main text-white px-5 py-2 rounded-xl drop-shadow-xl hover:bg-[#fff] hover:text-brown-main border hover:border-brown-main justify-center mt-5">Buat Pesanan
-      </button>
-      <!-- tombol kembali -->
-      <a href=""
-        class="bg-brown-main text-white px-5 py-2 rounded-xl drop-shadow-xl hover:bg-[#fff] hover:text-brown-main border hover:border-brown-main justify-center">Kembali
-      </a>
+      <!-- Detail Undangan -->
+      <div class="grid grid-cols-[50%_50%] gap-40 justify-center">
+        <!-- Desain -->
+        <div>
+          <div class="flex items-center flex-col">
+            <h2 class="text-center"><br>Desain dan Pembayaran</h2>
+            <hr class="border-b-2 border-brown-enzo w-full mb-4">
+          </div>
+
+          <div class="grid grid-rows-4 gap-5">
+
+            <div class="flex items-center flex-col">
+              <label for="invitation_desain">Desain</label>
+              <input type="file" id="invitation_desain" name="invitation_desain" 
+                accept=".jpg,.jpeg,.png,.pdf" required
+                class=" text-[#9ca3af] outline-none border border-[#e0e0e0] bg-[#f0f0f0] w-80 px-0 py-0.45 sm:py-0.45 md:py-0.45 lg:py-0.45">
+              @error('invitation_desain')
+              <small class="text-danger">{{ $message }}</small>
+              @enderror
+            </div>
+
+            <div class="flex items-center flex-col">
+              <label for="groom_nickname">Progress</label>
+              <input type="text" id="groom_nickname" name="groom_nickname" value="{{ old('groom_nickname') }}" required
+                placeholder="Nama Panggilan"
+                class="outline-none border border-[#e0e0e0] bg-[#f0f0f0] w-80 rounded-xl px-2 py-0.5 sm:py-0.5 md:py-0.5 lg:py-0.5">
+              @error('groom_nickname')
+              <small class="text-danger">{{ $message }}</small>
+              @enderror
+            </div>
+
+            <div class="flex items-center flex-col">
+              <label for="payment_status">Status Bayar</label>
+              <select id="payment_status" name="payment_status"
+                class="form-control outline-none border border-[#e0e0e0] bg-[#f0f0f0] w-80 rounded-xl px-2 py-1 sm:py-1 md:py-1 lg:py-1" required>
+                <option value="Belum Bayar">Belum Bayar</option>
+                <option value="DP 1">DP 1</option>
+                <option value="DP 2">DP 2</option>
+                <option value="Lunas">Lunas</option>
+              </select>
+              @error('payment_status')
+              <small class="text-danger">{{ $message }}</small>
+              @enderror
+            </div>
+
+            <div class="flex items-center flex-col">
+              <label for="groom_mother">Tanggal Pembayaran</label>
+              <input type="date" id="groom_mother" name="groom_mother" value="{{ old('groom_mother') }}" required
+                class="text-[#9ca3af] outline-none border border-[#e0e0e0] bg-[#f0f0f0] w-80 rounded-xl px-2 py-0.5 sm:py-0.5 md:py-0.5 lg:py-0.5">
+              @error('groom_mother')
+              <small class="text-danger">{{ $message }}</small>
+              @enderror
+            </div>
+          </div>
+        </div>
+
+        <!-- Informasi Tambahan -->
+        <div>
+          <div class="flex items-center flex-col">
+            <h2 class="text-center"><br>Informasi Tambahan</h2>
+            <hr class="border-b-2 border-brown-enzo w-full mb-4">
+          </div>
+          <div class="grid grid-rows-4 gap-5">
+            <div class="flex items-center flex-col">
+              <label for="item_price">Harga /Pcs</label>
+              <input type="text" id="item_price" name="item_price" value="{{ old('item_price') }}" required
+                placeholder="Harga /Pcs"
+                class="outline-none border border-[#e0e0e0] bg-[#f0f0f0] w-80 rounded-xl px-2 py-0.5 sm:py-0.5 md:py-0.5 lg:py-0.5">
+              @error('item_price')
+              <small class="text-danger">{{ $message }}</small>
+              @enderror
+            </div>
+
+            <div class="flex items-center flex-col">
+              <label for="additional_items">Printilan</label>
+              <input type="text" id="additional_items" name="additional_items" value="{{ old('additional_items') }}" required
+                placeholder="Printilan"
+                class="outline-none border border-[#e0e0e0] bg-[#f0f0f0] w-80 rounded-xl px-2 py-0.5 sm:py-0.5 md:py-0.5 lg:py-0.5">
+              @error('additional_items')
+              <small class="text-danger">{{ $message }}</small>
+              @enderror
+            </div>
+
+            <div class="flex items-center flex-col">
+              <label for="expedition">Ekspedisi</label>
+              <input type="text" id="expedition" name="expedition" value="{{ old('expedition') }}" required
+                placeholder="Ekspedisi"
+                class="outline-none border border-[#e0e0e0] bg-[#f0f0f0] w-80 rounded-xl px-2 py-0.5 sm:py-0.5 md:py-0.5 lg:py-0.5">
+              @error('expedition')
+              <small class="text-danger">{{ $message }}</small>
+              @enderror
+            </div>
+
+            <div class="flex items-center flex-col">
+              <label for="client_acc">Acc Client</label>
+              <select id="client_acc" name="client_acc"
+                class="form-control outline-none border border-[#e0e0e0] bg-[#f0f0f0] w-80 rounded-xl px-2 py-1 sm:py-1 md:py-1 lg:py-1" required>
+                <option value="Acc">Acc</option>
+                <option value="Belum">Belum</option>
+              </select>
+              @error('client_acc')
+              <small class="text-danger">{{ $message }}</small>
+              @enderror
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="button-container">
+        <!-- Submit Button -->
+        <button type="submit"
+                class="bg-brown-main text-white px-5 py-2 rounded-xl drop-shadow-xl hover:bg-[#fff] hover:text-brown-main border hover:border-brown-main justify-center mt-5">
+          Perbarui Pesanan
+        </button>
+        <!-- tombol kembali -->
+        <a href=""
+           class="bg-brown-main text-white px-10 py-[11px] rounded-xl drop-shadow-xl hover:bg-[#fff] hover:text-brown-main border hover:border-brown-main justify-center ml-8">
+          Kembali
+        </a>
+      </div>
+      
     </form>
   </div>
 </body>
