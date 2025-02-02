af0fcbd84d297c6f8c5381662e33051e38ad421b
Fathoni1509 <habibifathoni1509@gmail.com>
2025-02-02 21:35:18 +0700

    responsive dashboard user

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,21 +2,21 @@
   @section('title', 'User Dashboard')
   @section('konten')
   <!-- Main Container -->
-  <div class="ml-[20%] bg-green-light">
+  <div class="ml-[50px] lg:ml-[20%] bg-green-light">
     <!-- main content -->
     <div class="container h-screen">
       <div class="h-screen">
-        <div class="grid grid-rows-[100px_auto]">
+        <div class="grid grid-rows-[70px_auto] lg:grid-rows-[100px_auto]">
 
           <div class="flex flex-col justify-center items-center bg-green-main/20">
-            <h1 class="font-cookie text-5xl">Hello, {{ auth()->user()->name }}</h1>
-            <p><strong>This is the user dashboard page, accessible to regular users.</strong></p>
+            <h1 class="font-cookie text-3xl lg:text-5xl">Hello, {{ auth()->user()->name }}</h1>
+            <p class="text-xs lg:text-base"><strong>Welcome to Enzo Creatives</strong></p>
           </div>
 
-          <div class="grid grid-rows-[350px_auto]">
+          <div class="grid grid-rows-[520px_auto] lg:grid-rows-[350px_auto]">
             <div class="flex items-center justify-center">
-              <div class="grid grid-cols-4 px-4 py-4 gap-8">
-                <div class="carousel rounded-lg w-[210px] h-[280px] grid grid-rows-[1fr_30px] overflow-hidden relative hover:scale-110 transition duration-300 shadow-lg">
+              <div class="grid grid-cols-2 p-3 lg:grid-cols-4 lg:p-4 gap-4 lg:gap-8">
+                <div class="carousel rounded-lg w-[130px] h-[210px] lg:w-[210px] lg:h-[280px] grid grid-rows-[1fr_30px] overflow-hidden relative hover:scale-110 transition duration-300 shadow-lg">
                   <a href="{{ route('user.orders.invitation.create') }}">
                     <div class="carousel-inner flex w-[300%] h-full hover:animate-carousel">
                       <div class="carousel-item relative flex-shrink-0 w-1/3 h-full ">
@@ -38,13 +38,13 @@
                     </div>
       
                     <a href="{{ route('user.orders.invitation.create') }}"
-                      class=" text-black font-medium rounded-b-lg bg-green-main/50 hover:bg-green-main/90 hover:text-cream transition duration-300 text-center flex items-center justify-center">
+                      class=" text-black font-medium rounded-b-lg bg-green-main/50 hover:bg-green-main/90 hover:text-cream transition duration-300 text-center flex items-center justify-center text-xs lg:text-base">
                       Order Invitation
                     </a>
                   </a>
                 </div>
 
-                <div class="carousel rounded-lg w-[210px] h-[280px] grid grid-rows-[1fr_30px] overflow-hidden relative hover:scale-110 transition duration-300 shadow-lg">
+                <div class="carousel rounded-lg w-[130px] h-[210px] lg:w-[210px] lg:h-[280px] grid grid-rows-[1fr_30px] overflow-hidden relative hover:scale-110 transition duration-300 shadow-lg">
                     <a href="{{ route('user.orders.souvenir.create') }}">
                   <div class="carousel-inner flex w-[300%] h-full hover:animate-carousel">
                     <div class="carousel-item relative flex-shrink-0 w-1/3 h-full ">
@@ -66,13 +66,13 @@
                   </div>
     
                   <a href="{{ route('user.orders.souvenir.create') }}"
-                    class=" text-black font-medium rounded-b-lg bg-green-main/50 hover:bg-green-main/90 hover:text-cream transition duration-300 text-center flex items-center justify-center">
+                    class=" text-black font-medium rounded-b-lg bg-green-main/50 hover:bg-green-main/90 hover:text-cream transition duration-300 text-center flex items-center justify-center text-xs lg:text-base">
                     Order Souvenir
                   </a>
                 </a>
                 </div>
     
-                <div class="carousel rounded-lg w-[210px] h-[280px] grid grid-rows-[1fr_30px] overflow-hidden relative hover:scale-110 transition duration-300 shadow-lg">
+                <div class="carousel rounded-lg w-[130px] h-[210px] lg:w-[210px] lg:h-[280px] grid grid-rows-[1fr_30px] overflow-hidden relative hover:scale-110 transition duration-300 shadow-lg">
                     <a href="{{ route('user.orders.packaging.create') }}">
                   <div class="carousel-inner flex w-[300%] h-full hover:animate-carousel">
                     <div class="carousel-item relative flex-shrink-0 w-1/3 h-full ">
@@ -94,13 +94,13 @@
                   </div>
     
                   <a href="{{ route('user.orders.packaging.create') }}"
-                    class=" text-black font-medium rounded-b-lg bg-green-main/50 hover:bg-green-main/90 hover:text-cream transition duration-300 text-center flex items-center justify-center">
+                    class=" text-black font-medium rounded-b-lg bg-green-main/50 hover:bg-green-main/90 hover:text-cream transition duration-300 text-center flex items-center justify-center text-xs lg:text-base">
                     Order Packaging
                   </a>
                 </a>
                 </div>
     
-                <div class="carousel rounded-lg w-[210px] h-[280px] grid grid-rows-[1fr_30px] overflow-hidden relative hover:scale-110 transition duration-300 shadow-lg">
+                <div class="carousel rounded-lg w-[130px] h-[210px] lg:w-[210px] lg:h-[280px] grid grid-rows-[1fr_30px] overflow-hidden relative hover:scale-110 transition duration-300 shadow-lg">
                   <div class="carousel-inner flex w-[300%] h-full hover:animate-carousel">
                     <div class="carousel-item relative flex-shrink-0 w-1/3 h-full ">
                       <div class="absolute inset-0 bg-black/40 hover:bg-black/10"></div>
@@ -121,7 +121,7 @@
                   </div>
     
                   <a href="#"
-                    class=" text-black font-medium rounded-b-lg bg-green-main/50 hover:bg-green-main/90 hover:text-cream transition duration-300 text-center flex items-center justify-center">
+                    class=" text-black font-medium rounded-b-lg bg-green-main/50 hover:bg-green-main/90 hover:text-cream transition duration-300 text-center flex items-center justify-center text-xs lg:text-base">
                     Order Seminarkit
                   </a>
     
@@ -131,45 +131,45 @@
               </div>
             </div>
 
-            <div class="grid grid-cols-[250px_330px_auto] gap-5 overflow-hidden px-8 py-3">
-              <a href="https://maps.app.goo.gl/dSC6Z8tvWC19vseSA" target="_blank" class="w-[250px] h-full overflow-hidden rounded-md relative hover:scale-105 transition transform duration-300">
+            <div class="grid grid-cols-[140px_140px] lg:grid-cols-[250px_330px_auto] gap-2 pt-2 pb-3 px-3 lg:gap-5 overflow-hidden lg:px-8 lg:py-3 place-content-center">
+              <a href="https://maps.app.goo.gl/dSC6Z8tvWC19vseSA" target="_blank" class="w-[140px] lg:w-[250px] h-full overflow-hidden rounded-md relative hover:scale-105 transition transform duration-300">
                 <div class="absolute bottom-0 bg-black bg-opacity-60 w-full px-2 py-1">
-                  <p class="text-white text-center">Lihat Lokasi</p>
+                  <p class="text-white text-center text-xs lg:text-base">Lihat Lokasi</p>
                 </div>
                 <img src="{{ asset('img/enzo-office.jpg') }}" alt="" class="w-full h-full object-cover">
               </a>
-              <a href="https://www.detik.com/jateng/berita/d-6453925/suvenir-tasyakuran-kaesang-erina-dibuat-perajin-sukoharjo" target="_blank" class="w-[330px] h-[190px] overflow-hidden rounded-md relative hover:scale-105 transition transform duration-300">
-                <div class="absolute bottom-0 bg-black bg-opacity-60 w-full px-2 py-1">
+              <a href="https://www.detik.com/jateng/berita/d-6453925/suvenir-tasyakuran-kaesang-erina-dibuat-perajin-sukoharjo" target="_blank" class="w-[140px] h-full lg:w-[330px] lg:h-[190px] overflow-hidden rounded-md relative hover:scale-105 transition transform duration-300">
+                <div class="absolute bottom-0 bg-black bg-opacity-60 w-full p-1">
                   <p class="text-xs text-white mb-0.5">detik.com</p>
-                  <p class="text-white">Suvenir Tasyakuran Kaesang-Erina...</p>
+                  <p class="text-white text-xs lg:text-base">Suvenir Tasyakuran Kaesang...</p>
                 </div>
                 <img src="{{ asset('img/souvenir-kaesang.jpeg') }}" alt="" class="w-full h-full object-cover">
               </a>
-              <div class="w-full h-full grid grid-rows-2 gap-3">
-                <button type="button" id="button-panduan" class="bg-green-main/40 w-full h-full rounded-lg p-3 flex flex-row gap-2 justify-center items-center tracking-wider hover:bg-green-main/70 hover:text-white hover:scale-105 transition transform duration-300">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
+              <div class="w-[288px] lg:w-full h-full grid grid-rows-2 gap-3 place-items-center">
+                <button type="button" id="button-panduan" class="bg-green-main/40 w-[180px] h-[40px] lg:w-full lg:h-full rounded-lg p-3 flex flex-row gap-2 justify-center items-center tracking-wider hover:bg-green-main/70 hover:text-white hover:scale-105 transition transform duration-300 mt-3 lg:mt-0">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 lg:size-8">
                     <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
                   </svg>
-                  <p class="text-2xl">Panduan Pesan</p>
+                  <p class="text-sm lg:text-2xl">Panduan Pesan</p>
                 </button>
-                <a href="" class="bg-green-main/40 w-full h-full rounded-lg p-3 flex flex-row gap-2 justify-center items-center tracking-wider hover:bg-green-main/70 hover:text-white hover:scale-105 transition transform duration-300">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
+                <a href="" class="bg-green-main/40 w-[180px] h-[40px] lg:w-full lg:h-full rounded-lg p-3 flex flex-row gap-2 justify-center items-center tracking-wider hover:bg-green-main/70 hover:text-white hover:scale-105 transition transform duration-300">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 lg:size-8">
                     <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
                   </svg>
-                  <p class="text-2xl">Hubungi Kami</p>
+                  <p class="text-sm lg:text-2xl">Hubungi Kami</p>
                 </a>
               </div>
             </div>
 
             <div id="page-panduan" class="hidden">
-                <div tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-screen bg-black/50">
-                  <div class="opacity-0 mr-1 h-8 w-8"></div>
+                <div tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-screen bg-black/50 px-2 lg:px-0">
+                  <div class="opacity-0 mr-0.5 lg:mr-1 h-8 w-8"></div>
 
-                  <div class="relative p-1 w-full max-w-md max-h-full">
+                  <div class="relative p-3 lg:p-1 w-full max-w-md max-h-full">
                       <!-- Modal content -->
                       <div class="relative bg-white/70 backdrop-blur-lg rounded-lg shadow-sm">
                               <!-- Modal header -->
-                              <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-900">
+                              <div class="flex items-center justify-between p-4 border-b rounded-t border-gray-900">
                                   <h3 class="text-lg font-semibold text-gray-900">
                                       Panduan Pemesanan
                                   </h3>
@@ -181,7 +181,7 @@
                                   </button>
                               </div>
                               <!-- Modal body -->
-                              <div class="p-4 md:p-5 border-b border-gray-900">
+                              <div class="p-4 md:p-5 border-b border-gray-900 text-sm lg:text-base">
                                 <ol class="ms-3.5 mb-3 list-decimal space-y-3">
                                   <li>Pada menu 'Dashboard', pilih menu order jenis item yang hendak dipesan.</li>
                                   <li>Isi form order dengan data yang lengkap dan benar.</li>
@@ -193,8 +193,8 @@
                           </div>
                   </div>
 
-                  <button id="next-guide" class="relative bg-white/70 backdrop-blur-xl rounded-full shadow-sm ml-1 flex items-center justify-center text-gray-900 hover:bg-gray-200 text-sm h-8 w-8">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                  <button id="next-guide" class="relative bg-white/70 backdrop-blur-xl rounded-full shadow-sm ml-0.5 lg:ml-1 flex items-center justify-center text-gray-900 hover:bg-gray-200 text-sm h-6 w-7 lg:h-8 lg:w-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 lg:size-6">
                       <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
                     </svg>
                   </button>
@@ -202,20 +202,20 @@
             </div>
 
             <div id="page-panduan-next" class="hidden">
-                <div tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-screen bg-black/50">
+                <div tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-screen bg-black/50 px-2 lg:px-0">
 
-                  <button id="previous-guide" class="relative bg-white/70 backdrop-blur-xl rounded-full shadow-sm mr-1 flex items-center justify-center text-gray-900 hover:bg-gray-200 text-sm h-8 w-8">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                  <button id="previous-guide" class="relative bg-white/70 backdrop-blur-xl rounded-full shadow-sm mr-0.5 lg:mr-1 flex items-center justify-center text-gray-900 hover:bg-gray-200 text-sm h-6 w-7 lg:h-8 lg:w-8"">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 lg:size-6">
                       <path fill-rule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clip-rule="evenodd" />
                     </svg>
 
                   </button>
 
-                  <div class="relative p-1 w-full max-w-md max-h-full">
+                  <div class="relative p-3 lg:p-1 w-full max-w-md max-h-full">
                       <!-- Modal content -->
                       <div class="relative bg-white/70 backdrop-blur-lg rounded-lg shadow-sm">
                               <!-- Modal header -->
-                              <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-900">
+                              <div class="flex items-center justify-between p-4 border-b rounded-t border-gray-900">
                                   <h3 class="text-lg font-semibold text-gray-900">
                                       Proses Produksi
                                   </h3>
@@ -227,7 +227,7 @@
                                   </button>
                               </div>
                               <!-- Modal body -->
-                              <div class="p-4 md:p-5 border-b border-gray-900">
+                              <div class="p-4 md:p-5 border-b border-gray-900 text-sm lg:text-base">
                                 <ol class="ms-3.5 mb-3 list-decimal space-y-3">
                                   <li>Pembayaran akan dibagi menjadi 3 tahap, yaitu DP 1, DP 2, dan pelunasan.</li>
                                   <li>Desain produk akan diproses setelah Anda melakukan pembayaran DP 1.</li>
@@ -241,7 +241,7 @@
                           </div>
                   </div>
 
-                  <div class="opacity-0 ml-1 h-8 w-8"></div>
+                  <div class="opacity-0 ml-0.5 lg:ml-1 h-8 w-8"></div>
                 </div>
             </div>
             
--- resources/views/user/sidebar_user.blade.php
+++ resources/views/user/sidebar_user.blade.php
@@ -11,53 +11,63 @@
   <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
 </head>
 
-<body class="font-mont">
-  <aside class="z-40 w-1/5 fixed top-0 left-0">
-    <div class="bg-green-main min-h-screen">
+<body class="font-mont bg-green-light">
+  <aside class="z-40 lg:w-1/5 md:w-[50px] w-[50px] fixed top-0 left-0 h-full lg:min-h-screen bg-green-main">
+    <div>
       <ul class="space-y-5 py-10">
         <li>
           <div class="flex items-center space-x-3">
-            <div class="w-10 h-10 bg-cream rounded-full flex items-center justify-center ml-4">
-              <span class="text-green-main font-medium">{{Str::substr(auth()->user()->name, 0, 1)}}</span>
+            <div class="hidden lg:block">
+              <div class="lg:w-10 lg:h-10 w-8 h-8 bg-cream rounded-full flex items-center justify-center ml-4">
+                <span class="text-green-main font-medium">{{Str::substr(auth()->user()->name, 0, 1)}}</span>
+              </div>
             </div>
-            <span style="letter-spacing: 3px" class="font-sans ms-3 text-2xl font-medium text-cream px-1">{{auth()->user()->name}}</span>
+
+            <!-- icon hamburger saat mobile -->
+            <div class="ml-4 lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-cream">
+                <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+              </svg>
+            </div>
+
+            <span style="letter-spacing: 3px" class="font-sans ms-3 text-2xl font-medium text-cream px-1 hidden lg:block">{{auth()->user()->name}}</span>
           </div>
         </li>
         <li>
-          <a href="/user/dashboard" style="letter-spacing: 3px" class="font-sans flex items-center py-3 px-4 w-5/6 rounded-r-2xl text-cream hover:bg-cream hover:text-green-main focus:bg-cream focus:text-green-main">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5 mr-2">
+          <a href="/user/dashboard" style="letter-spacing: 3px" class="font-sans flex items-center pl-3 pr-1 py-2 w-[45px] lg:py-3 lg:px-4 lg:w-5/6 rounded-r-2xl text-cream hover:bg-cream hover:text-green-main">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 lg:size-5 lg:mr-2">
               <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
               <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
             </svg>
-            <span>Dashboard</span>
+            <span class="hidden lg:block">Dashboard</span>
           </a>
         </li>
         <li>
-          <a href="/user/orders" style="letter-spacing: 3px" class="font-sans flex items-center py-3 px-4 w-5/6 rounded-r-2xl text-cream hover:bg-cream hover:text-green-main focus:bg-cream focus:text-green-main">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5 mr-2">
+          <a href="/user/orders" style="letter-spacing: 3px" class="font-sans flex items-center pl-3 pr-1 py-2 w-[45px] lg:py-3 lg:px-4 lg:w-5/6 rounded-r-2xl text-cream hover:bg-cream hover:text-green-main">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 lg:size-5 lg:mr-2">
               <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
             </svg>
-            <span>Pesanan Saya</span>
+            <span class="hidden lg:block">Pesanan Saya</span>
           </a>
         </li>
         <li>
-          <a href="" style="letter-spacing: 3px" class="font-sans flex items-center py-3 px-4 w-5/6 rounded-r-2xl text-cream hover:bg-cream hover:text-green-main">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5 mr-2">
+          <a href="" style="letter-spacing: 3px" class="font-sans flex items-center pl-3 pr-1 py-2 w-[45px] lg:py-3 lg:px-4 lg:w-5/6 rounded-r-2xl text-cream hover:bg-cream hover:text-green-main">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 lg:size-5 lg:mr-2">
               <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
             </svg>
-            <span>Contact</span>
+            <span class="hidden lg:block">Contact</span>
           </a>
         </li>
         <li>
-        <form action="{{route('logout')}}" method="POST">
-        @csrf
-            <button type="submit" style="letter-spacing: 3px" class="font-sans flex items-center py-3 px-4 w-5/6 rounded-r-2xl text-cream hover:bg-cream hover:text-green-main">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 mr-2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
-              </svg>
-              <span>Logout</span>
-            </button>
-        </form>
+          <form action="{{route('logout')}}" method="POST">
+          @csrf
+              <button type="submit" style="letter-spacing: 3px" class="font-sans flex items-center pl-3 pr-1 py-2 w-[45px] lg:py-3 lg:px-4 lg:w-5/6 rounded-r-2xl text-cream hover:bg-cream hover:text-green-main">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 lg:size-5 lg:mr-2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+                </svg>
+                <span class="hidden lg:block">Logout</span>
+              </button>
+          </form>
         </li>
       </ul>
     </div>
