fed84cefa9f139bb9da380d525f20224f88cc823
hnndhr <hananadhira1475@gmail.com>
2025-02-05 22:22:03 +0700

    header dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -10,7 +10,7 @@
 
           <div class="flex flex-col justify-center items-center bg-green-main/20">
             <h1 class="font-quattro text-3xl lg:text-5xl font-bold">Hello, {{ auth()->user()->name }}</h1>
-            <p class="font-azeret mt-1">Welcome to Enzo Creatives. Let's craft unforgettable memories together!</p>
+            <p class="font-azeret mt-1 text-center">Welcome to Enzo Creatives!</p>
           </div>
 
           <div class="grid grid-rows-[520px_auto] lg:grid-rows-[350px_auto]">
