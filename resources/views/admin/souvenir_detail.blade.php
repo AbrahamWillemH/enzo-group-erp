7eb3e9841820b4ae59ad99a0c4d350c37eb1d768
Abe <117052559+AbrahamWillemH@users.noreply.github.com>
2025-02-18 02:48:06 +0700

    fix ukuran jadi souvenir integrated

--- resources/views/admin/souvenir_detail.blade.php
+++ resources/views/admin/souvenir_detail.blade.php
@@ -163,7 +163,7 @@
                             </tr>
                             <tr class="bg-green-shadow/20 h-[60px] hover:bg-green-shadow/40 transition-all duration-300">
                                 <td class="w-[35%] px-4 py-2">Ukuran Jadi</td>
-                                <td class="px-4 py-2"></td>
+                                <td class="px-4 py-2">{{$souvenir->size_fix}}</td>
                             </tr>
 
                         </tbody>
