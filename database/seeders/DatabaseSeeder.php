7d0f7d00d00c5f5d746fcf201f36091cfaf0db4a
Abraham Willem H <abraham120604@gmail.com>
2025-03-13 00:06:19 +0700

    feat productioncapacity on the way

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,31 +13,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'test123',
-            'role' => 'user',
-        ]);
-        User::factory()->create([
-            'name' => 'admin1',
-            'email' => 'admin1@gmail.com',
-            'password' => 'enZO12345!!',
-            'role' => 'admin',
-        ]);
-        User::factory()->create([
-            'name' => 'admin2',
-            'email' => 'admin1@gmail.com',
-            'password' => 'enZO12345!!',
-            'role' => 'admin',
-        ]);
-        User::factory()->create([
-            'name' => 'admin3',
-            'email' => 'admin1@gmail.com',
-            'password' => 'enZO12345!!',
-            'role' => 'admin',
-        ]);
+        // // User::factory(10)->create();
+        //
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        //     'password' => 'test123',
+        //     'role' => 'user',
+        // ]);
+        // User::factory()->create([
+        //     'name' => 'admin1',
+        //     'email' => 'admin1@gmail.com',
+        //     'password' => 'enZO12345!!',
+        //     'role' => 'admin',
+        // ]);
+        // User::factory()->create([
+        //     'name' => 'admin2',
+        //     'email' => 'admin1@gmail.com',
+        //     'password' => 'enZO12345!!',
+        //     'role' => 'admin',
+        // ]);
+        // User::factory()->create([
+        //     'name' => 'admin3',
+        //     'email' => 'admin1@gmail.com',
+        //     'password' => 'enZO12345!!',
+        //     'role' => 'admin',
+        // ]);
+        $this->call([
+                  ProductionCapacitySeeder::class,
+              ]);
     }
 }
